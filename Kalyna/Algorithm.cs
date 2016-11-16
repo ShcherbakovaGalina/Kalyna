9f95a5473a09cbe109268fbfe9dc5d63e8001d8e
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2016-11-16 02:19:20 +0200

    First encryption

--- Kalyna/Algorithm.cs
+++ Kalyna/Algorithm.cs
@@ -6,12 +6,16 @@ namespace Kalyna
 {
     public class Algorithm
     {
+        public bool UseLog { get; set; } = false;
+
+        private List<Block> RoundsKeys { get; set; } = new List<Block>();
+
         public void Log(string message, Block block)
         {
             Console.WriteLine($"{message,-30} {new BigInteger(block.Data.ToArray()).ToString("X32")}");
         }
 
-        private Block GenerateKt(Block key, bool useLog = false)
+        private Block GenerateKt(Block key)
         {
             var kt = new Block
             {
@@ -22,63 +26,62 @@ namespace Kalyna
             };
 
             kt.AddRoundKey(key);
-            if (useLog)
+            if (UseLog)
                 Log("state[0].add_rkey:", kt);
 
             kt.SubBytes();
-            if (useLog)
+            if (UseLog)
                 Log("state[0].s_box:", kt);
 
             kt.ShiftRows();
-            if (useLog)
+            if (UseLog)
                 Log("state[0].s_row:", kt);
 
             kt.MixColumns();
-            if (useLog)
+            if (UseLog)
                 Log("state[0].m_col:", kt);
 
             kt.Xor(key);
-            if (useLog)
+            if (UseLog)
                 Log("state[0].xor_rkey:", kt);
 
             kt.SubBytes();
-            if (useLog)
+            if (UseLog)
                 Log("state[0].s_box:", kt);
 
             kt.ShiftRows();
-            if (useLog)
+            if (UseLog)
                 Log("state[0].s_row:", kt);
 
             kt.MixColumns();
-            if (useLog)
+            if (UseLog)
                 Log("state[0].m_col:", kt);
 
             kt.AddRoundKey(key);
-            if (useLog)
+            if (UseLog)
                 Log("state[0].add_rkey:", kt);
 
             kt.SubBytes();
-            if (useLog)
+            if (UseLog)
                 Log("state[0].s_box:", kt);
 
             kt.ShiftRows();
-            if (useLog)
+            if (UseLog)
                 Log("state[0].s_row:", kt);
 
             kt.MixColumns();
-            if (useLog)
+            if (UseLog)
                 Log("state[0].m_col:", kt);
 
             return kt;
         }
 
-        public List<Block> GenerateRoundsKeys(Block key, bool useLog = false)
+        public List<Block> GenerateRoundsKeys(Block key)
         {
             Log("Key", key);
 
-            var keys = new List<Block>();
             for (var i = 0; i <= 10; i++)
-                keys.Add(new Block());
+                RoundsKeys.Add(new Block());
 
             // Even keys
 
@@ -90,79 +93,128 @@ namespace Kalyna
             //        0xA1, 0x5B, 0x77, 0x3B, 0x65, 0x1F, 0x2F, 0x86
             //    }
             //};
-            var kt = GenerateKt(key, useLog);
+            var kt = GenerateKt(key);
             Log("KT", kt);
 
             for (var i = 0; i <= 10; i += 2)
             {
-                if (useLog)
+                if (UseLog)
                     Console.WriteLine();
-                var roundKey = keys[i];
+                var roundKey = RoundsKeys[i];
                 roundKey.Data = new List<byte>(StaticTables.V);
                 roundKey.ShiftLeft(i / 2);
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].ShiftLeft (tmv):", roundKey);
 
                 var keyCopy = new Block(key);
                 keyCopy.RotateRight(32 * i);
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].Rotate (id):", keyCopy);
 
                 roundKey.AddRoundKey(kt);
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].add_rkey (tmv):", roundKey);
                 var copy = new Block(roundKey);
 
                 roundKey.AddRoundKey(keyCopy);
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].add_rkey (kt_round):", roundKey);
 
                 roundKey.SubBytes();
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].s_box:", roundKey);
 
                 roundKey.ShiftRows();
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].s_row:", roundKey);
 
                 roundKey.MixColumns();
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].m_col:", roundKey);
 
                 roundKey.Xor(copy);
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].xor_rkey (kt_round):", roundKey);
 
                 roundKey.SubBytes();
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].s_box:", roundKey);
 
                 roundKey.ShiftRows();
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].s_row:", roundKey);
 
                 roundKey.MixColumns();
-                if (useLog)
+                if (UseLog)
                     Log($"state[{i}].m_col:", roundKey);
 
                 roundKey.AddRoundKey(copy);
-                if (useLog)
+                if (UseLog)
                 {
                     Log($"state[{i}].add_rkey (tmv):", copy);
                     Log($"state[{i}].add_rkey (kt_round):", roundKey);
                 }
-                keys[i] = roundKey;
+                RoundsKeys[i] = roundKey;
             }
 
             // Odd keys
             for (var i = 1; i <= 9; i += 2)
             {
-                keys[i].Data = keys[i - 1].Data;
-                keys[i].RotateLeft(64);
+                RoundsKeys[i].Data = RoundsKeys[i - 1].Data;
+                if (i == 7)
+                {
+                    
+                }
+                RoundsKeys[i].RotateLeft(64);
             }
 
-            return keys;
+            return RoundsKeys;
         }
 
+        public Block Encrypt(Block plainText, Block key)
+        {
+            var encryptedText = new Block(RoundsKeys[0]);
+            //encryptedText.AddRoundKey(RoundsKeys[0]);
+
+            for (var i = 1; i <= 9; i++)
+            {
+                if (UseLog)
+                    Console.WriteLine();
+
+                encryptedText.SubBytes();
+                if (UseLog)
+                    Log($"round[{i}].s_box:", encryptedText);
+
+                encryptedText.ShiftRows();
+                if (UseLog)
+                    Log($"round[{i}].s_row:", encryptedText);
+
+                encryptedText.MixColumns();
+                if (UseLog)
+                    Log($"round[{i}].m_col:", encryptedText);
+
+                encryptedText.Xor(RoundsKeys[i]);
+                if (UseLog)
+                    Log($"round[{i}].xor_rkey:", encryptedText);
+            }
+
+            encryptedText.SubBytes();
+            if (UseLog)
+                Log("round[10].s_box:", encryptedText);
+
+            encryptedText.ShiftRows();
+            if (UseLog)
+                Log("round[10].s_row:", encryptedText);
+
+            encryptedText.MixColumns();
+            if (UseLog)
+                Log("round[10].m_col:", encryptedText);
+
+            encryptedText.AddRoundKey(RoundsKeys[9]);
+            if (UseLog)
+                Log("round[10].add_rkey:", encryptedText);
+
+            return encryptedText;
+        }
     }
 }
--- Kalyna/Program.cs
+++ Kalyna/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Numerics;
 
 namespace Kalyna
 {
     internal static class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            var algorithm = new Algorithm();
+            var algorithm = new Algorithm
+            {
+                UseLog = true
+            };
             var key = new Block
             {
                 Data = new List<byte>
@@ -24,6 +26,16 @@ namespace Kalyna
             {
                 algorithm.Log(i.ToString(), keys[i]);
             }
+
+            var plainText = new Block
+            {
+                Data = new List<byte>
+                {
+                    31, 30, 29, 28, 27, 26, 25, 24,
+                    23, 22, 21, 20, 19, 18, 17, 16
+                }
+            };
+            algorithm.Encrypt(plainText, key);
         }
     }
 }
